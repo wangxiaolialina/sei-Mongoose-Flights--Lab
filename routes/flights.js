7c91db097afab04a5db29d176475c13721401e5d
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-06-07 16:34:48 -0400

    add show routes

--- routes/flights.js
+++ routes/flights.js
@@ -4,6 +4,8 @@ const flightsCtrl = require('../controller/flights');
 
 router.get('/', flightsCtrl.index);
 router.get('/new',flightsCtrl.new);
+router.get('/:id',flightsCtrl.show);
 router.post('/', flightsCtrl.create);
+
 
 module.exports = router;
