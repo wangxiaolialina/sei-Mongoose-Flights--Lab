81f09e0f75ec0fd222fa8115acc6e7ac39672c6c
Alina <AlinaWang@Alina-MacBook-Pro.local>
2020-06-07 14:22:34 -0400

    add new flight to flight ctrl add create flight to flight ctrl

--- controller/flights.js
+++ controller/flights.js
@@ -1,16 +1,31 @@
 const Flight = require('../models/flight');
 
 module.exports = {
-index,
-new: newFlight,
+  index,
+  new: newFlight,
+  create,
 }
 
 function index(req, res) {
-    Flight.find({}, function(err,flights) {
-      res.render('flights/index', { title: 'All Flights', flights });
+  Flight.find({}, function (err, flights) {
+    res.render('flights/index', {
+      title: 'All Flights',
+      flights
     });
-  }
+  });
+}
 
-  function newFlight(req, res) {
-    res.render('flights/new', { title: 'Add Flight' });
-  }
+function newFlight(req, res) {
+  res.render('flights/new', {
+    title: 'Add Flight'
+  });
+}
+
+function create(req, res) {
+  const flight = new Flight(req.body);
+  flight.save(function (err) {
+    if (err) return res.redirect('/flights/new');
+    
+    res.redirect('/flights');
+  });
+}
